3b6100f96fa0755f95992fb3a51535249d7d135c
Markus Jungbluth <markus228@users.noreply.github.com>
2017-02-11 17:38:20 +0100

    Changed docs

--- pollard_roh.py
+++ pollard_roh.py
@@ -2,16 +2,14 @@
 This is a simple, yet straight forward implementation of Pollard's rho algorithm for discrete logarithms
 It computes X such that G^X = H mod P.
 
-Too keep this implementation simple p must be a safe prime, such that
-there is a prime q for which p = 2q+1 holds true.
+p must be a safe prime, such that there is a prime q for which p = 2q+1 holds true.
 
 The algorithm was designed using a "Hase/Igel" (German: Rabbit/Hedgehog) approach.
 Meaning there are two independent computations of Pollard's rho at different speeds.
 The algorithm stops, when the slower "Igel" has overtaken the faster rabbit and their X values are equal.
-We call this a collision.
 
 This program was developed as part of an assignment in the lecture
-"Security and Cryptology" by Prof. Weber at Saarland University of Applied Sciences (htw saar)
+"Security and Cryptography" by Prof. Weber at Saarland University of Applied Sciences (htw saar)
 """
 
 
