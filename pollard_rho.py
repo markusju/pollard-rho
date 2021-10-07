2aa5a7191b71676fdc09dac91e599b9e0ee3ad4f
Markus Jungbluth <markusju@users.noreply.github.com>
2021-10-07 14:33:35 +0200

    Update pollard_rho.py

--- pollard_rho.py
+++ pollard_rho.py
@@ -3,13 +3,6 @@ This is a simple, yet straight forward implementation of Pollard's rho algorithm
 It computes X such that G^X = H mod P.
 
 p must be a safe prime, such that there is a prime q for which p = 2q+1 holds true.
-
-The algorithm was designed using a "Hase/Igel" (German: Rabbit/Hedgehog) approach.
-Meaning there are two independent computations of Pollard's rho at different speeds.
-The algorithm stops, when the slower "Igel" has overtaken the faster rabbit and their X values are equal.
-
-This program was developed as part of an assignment in the lecture
-"Security and Cryptography" by Prof. Weber at Saarland University of Applied Sciences (htw saar)
 """
 
 
