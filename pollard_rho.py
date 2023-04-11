4852c9381a5cc6af248bdd566cfe87970cf874a1
benaroya <laurent.benaroya@gmail.com>
2023-04-11 16:13:50 +0200

    modified algorithm such that it works with any prime, the final linear eqaution is handled with care, small cosmetic changes + python 3 code

--- pollard_rho.py
+++ pollard_rho.py
@@ -2,37 +2,12 @@
 This is a simple, yet straight forward implementation of Pollard's rho algorithm for discrete logarithms
 It computes X such that G^X = H mod P.
 
-p must be a safe prime, such that there is a prime q for which p = 2q+1 holds true.
+p does not need to be a safe prime.
 """
+from gmpy2 import invert, gcd
 
 
-def ext_euclid(a, b):
-    """
-    Extended Euclidean Algorithm
-    :param a:
-    :param b:
-    :return:
-    """
-    if b == 0:
-        return a, 1, 0
-    else:
-        d, xx, yy = ext_euclid(b, a % b)
-        x = yy
-        y = xx - (a / b) * yy
-        return d, x, y
-
-
-def inverse(a, n):
-    """
-    Inverse of a in mod n
-    :param a:
-    :param n:
-    :return:
-    """
-    return ext_euclid(a, n)[1]
-
-
-def xab(x, a, b, (G, H, P, Q)):
+def xab(x, a, b, params):
     """
     Pollard Step
     :param x:
@@ -40,17 +15,17 @@ def xab(x, a, b, (G, H, P, Q)):
     :param b:
     :return:
     """
+    G, H, P, Q = params
     sub = x % 3 # Subsets
 
     if sub == 0:
         x = x*G % P
         a = (a+1) % Q
-
-    if sub == 1:
-        x = x * H % P
-        b = (b + 1) % Q
-
-    if sub == 2:
+    elif sub == 1:
+        x = x*H % P
+        b = (b+1) % Q
+    else:
+        # sub == 2:
         x = x*x % P
         a = a*2 % Q
         b = b*2 % Q
@@ -63,21 +38,13 @@ def pollard(G, H, P):
     # P: prime
     # H:
     # G: generator
-    Q = (P - 1) / 2  # sub group
+    Q = (P - 1)  # multiplicative sub group order
 
+    x, a, b = 1, 0, 0
+    X, A, B = x, a, b
 
-    x = G*H
-    a = 1
-    b = 1
-
-    X = x
-    A = a
-    B = b
-
-    # Do not use range() here. It makes the algorithm amazingly slow.
-    for i in xrange(1, P):
-        # Who needs pass-by reference when you have Python!!! ;)
-
+    # for i in range(1, P):
+    while True:
         # Hedgehog
         x, a, b = xab(x, a, b, (G, H, P, Q))
 
@@ -88,20 +55,30 @@ def pollard(G, H, P):
         if x == X:
             break
 
-
-    nom = a-A
-    denom = B-b
-
-    print nom, denom
+    num = a-A
+    denum = B-b
 
     # It is necessary to compute the inverse to properly compute the fraction mod q
-    res = (inverse(denom, Q) * nom) % Q
+    # find gcd before solving linear equation denum*res = num mod Q
+    gcdz = gcd(num, denum, Q)
+    # print(f'gcds = {gcdz}')
+    if gcdz == 1:  # standard solution
+        res = invert(denum, Q)*num % Q  # divm(num, denum, Q)  # (inverse(denom, Q) * nom) % Q
+    else:
+        # needs a little bit more work
+        # divide by gcd
+        modz = Q//gcdz
+        num = num//gcdz
+        denum = denum//gcdz
 
-    # I know this is not good, but it does the job...
-    if verify(G, H, P, res):
-        return res
-
-    return res + Q
+        # baseline solution
+        res0 = invert(denum, modz)*num % modz
+        # check in solutions of the shape (denum/gcd)*res = (num/gcd) mod (Q/gcd) + k * (Q/gcd) (k in [0, gcd[) 
+        for k in range(gcdz):
+            res = res0+k*modz
+            if verify(G, H, P, res):
+                break
+    return res
 
 
 def verify(g, h, p, x):
@@ -115,26 +92,26 @@ def verify(g, h, p, x):
     """
     return pow(g, x, p) == h
 
-M = 424242
 
-args = [
-    (2, 11, 59),
-    (2, M, 5041259),
-    (5, M, 87993167),
-    (2, M, 1726565507),
-    (7, M, 24455596799),
-    (5, M, 368585361623),
-    (11, M, 4520967464159),
-    (5, M, 66008980226543),
-    (5, M, 676602320278583),
-    (2, M, 2075952270932339),
-    (7, M, 21441211962585599)
-]
+if __name__ == '__main__':
+    M = 424242
 
-for arg in args:
-    res = pollard(*arg)
-    print arg, ': ', res
-    print "Validates: ", verify(arg[0], arg[1], arg[2], res)
-    print
+    args = [
+        (2, 11, 59),
+        (2, M, 5041259),
+        (5, M, 87993167),
+        (2, M, 1726565507),
+        (7, M, 24455596799),
+        (5, M, 368585361623),
+        (11, M, 4520967464159),
+        (5, M, 66008980226543),
+        (5, M, 676602320278583),
+        (2, M, 2075952270932339),
+        (7, M, 21441211962585599)
+    ]
 
-
+    for arg in args:
+        res = pollard(*arg)
+        print(arg, ': ', res)
+        print("Validates: ", verify(*arg, res))
+        print()
